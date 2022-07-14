0441567b4fbae943ff444b980af3163993c66a4e
Ilya Anatsky <anatskiy@mwnts.ru>
2022-07-14 17:47:58 +0300

    Added usage of zeroMQ

--- data_receiver.cpp
+++ data_receiver.cpp
@@ -5,7 +5,7 @@
 
 #include "boost/date_time/posix_time/posix_time.hpp"
 #include <boost/algorithm/hex.hpp>
-//#include <boost/uuid/detail/md5.hpp>
+#include <boost/uuid/detail/md5.hpp>
 #include <boost/circular_buffer.hpp>
 #include <boost/asio.hpp>
 #include <iostream>
@@ -28,17 +28,17 @@ constexpr auto min_data_len = (600 * sizeof(uint16_t));
 constexpr auto max_data_len = (1600 * sizeof(uint16_t));
 constexpr auto header_len = size_len + number_len + time_len + md5_len;
 constexpr auto max_buffer_size = header_len + max_data_len;
- 
-struct SReceiveStatistics 
+
+struct SReceiveStatistics
 {
     int received_number_packages;
     int procecced_number_packages;
     int dropped_number_packages;
     int errors_min_length;
-    int errors_received_length;    
+    int errors_received_length;
     int errors_md5_check;
 
-    SReceiveStatistics() 
+    SReceiveStatistics()
     {
         received_number_packages = 0;
         procecced_number_packages = 0;
@@ -62,53 +62,45 @@ struct SOneBuffer
 };
 
 static SReceiveStatistics recvStat;
-static std::atomic<int> processDataInd{ 0 };
-static std::atomic<int> receiveDataInd{ 1 };
+static std::atomic<int> processDataInd{0};
+static std::atomic<int> receiveDataInd{1};
 
 int getNextElement(int element, const int size)
-{    
+{
     return ((++element) == size ? 0 : element);
 }
 
-bool MD5Check(uint8_t* buffer_p)
-{    
-    // uint16_t dataSize = ntohs(*((uint16_t*)buffer_p));  
-    // boost::uuids::detail::md5 boost_md5;
-    // boost_md5.process_bytes(buffer_p + header_len, dataSize);
-    // boost::uuids::detail::md5::digest_type digest;
-    // boost_md5.get_digest(digest);
-    // uint8_t* md5_p = buffer_p + size_len + number_len + time_len;
-    // uint8_t* uint8_digest_p = reinterpret_cast<uint8_t*>(&digest);
-    
-    // return std::equal(md5_p, md5_p + md5_len, uint8_digest_p);
+bool MD5Check(uint8_t *buffer_p)
+{
+    uint16_t dataSize = ntohs(*((uint16_t*)buffer_p));
+    boost::uuids::detail::md5 boost_md5;
+    boost_md5.process_bytes(buffer_p + header_len, dataSize);
+    boost::uuids::detail::md5::digest_type digest;
+    boost_md5.get_digest(digest);
+    uint8_t* md5_p = buffer_p + size_len + number_len + time_len;
+    uint8_t* uint8_digest_p = reinterpret_cast<uint8_t*>(&digest);
 
-    return true;
+    return std::equal(md5_p, md5_p + md5_len, uint8_digest_p);
 }
 
-void receiveFromUDP(SConfigV& configval, std::vector<SOneBuffer>& circular_buffer)
-{   
-    // using namespace boost::asio;
-    // io_service service;
-    // ip::udp::socket sock(service);
-    // sock.open(ip::udp::v4());
-    // ip::udp::endpoint local_ep(ip::address::from_string(configval.local_ip), configval.local_port);
-    // sock.bind(local_ep);
-    // ip::udp::endpoint sender_ep;
-   
+void receiveFromNet(SConfigV &configval, std::vector<SOneBuffer> &circular_buffer)
+{
+    std::string tcp_addr_s = "tcp://" + configval.local_ip + ":" + std::to_string(configval.local_port);
+
     void *serverContext = zmq_ctx_new();
     void *serverSocket = zmq_socket(serverContext, ZMQ_REP);
-    int rc = zmq_bind(serverSocket, "tcp://192.168.1.117:5001");
-    assert (rc == 0);
+    int rc = zmq_bind(serverSocket, tcp_addr_s.c_str());
+    assert(rc == 0);
 
-    uint8_t writeBuffer[max_buffer_size] = { 0 };
+    uint8_t writeBuffer[max_buffer_size] = {0};
 
     for (;;)
     {
         // auto receivedSize = sock.receive_from(buffer(writeBuffer, max_buffer_size), sender_ep);
 
         zmq_msg_t request;
-		zmq_msg_init(&request);
-		auto receivedSize = zmq_msg_recv(&request, serverSocket, 0);
+        zmq_msg_init(&request);
+        auto receivedSize = zmq_msg_recv(&request, serverSocket, 0);
 
         if (receivedSize > 0)
         {
@@ -118,18 +110,21 @@ void receiveFromUDP(SConfigV& configval, std::vector<SOneBuffer>& circular_buffe
             zmq_msg_close(&request);
 
             // Send back
-            zmq_send(serverSocket, writeBuffer, msgSize, ZMQ_DONTWAIT);            
+            zmq_send(serverSocket, writeBuffer, msgSize, ZMQ_DONTWAIT);
         }
         else
         {
             // Todo some tasks
+            std::cout << "I can do some tasks!" << std::endl;
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
 
             continue;
         }
 
         recvStat.received_number_packages++;
 
-        int nextElement = getNextElement(receiveDataInd.load(), circular_buffer.size());        
+        int nextElement = getNextElement(receiveDataInd.load(), circular_buffer.size());
         if (nextElement != processDataInd.load())
         {
             // Check for MIN length
@@ -141,13 +136,13 @@ void receiveFromUDP(SConfigV& configval, std::vector<SOneBuffer>& circular_buffe
             }
 
             // Check for correct received length
-            uint16_t expectSize = ntohs(*((uint16_t*)writeBuffer)) + header_len;
+            uint16_t expectSize = ntohs(*((uint16_t *)writeBuffer)) + header_len;
             if (expectSize != (uint16_t)receivedSize)
             {
                 recvStat.errors_received_length++;
                 std::cout << "Error: incorrect received length,(receivedSize="
-                    << receivedSize << ") != (expectSize=" << expectSize
-                    << ")!" << std::endl;
+                          << receivedSize << ") != (expectSize=" << expectSize
+                          << ")!" << std::endl;
                 continue;
             }
             memcpy(circular_buffer[nextElement].buffer, writeBuffer, max_buffer_size);
@@ -157,12 +152,12 @@ void receiveFromUDP(SConfigV& configval, std::vector<SOneBuffer>& circular_buffe
         else
         {
             recvStat.dropped_number_packages++;
-        }        
+        }
     }
-    //sock.close();
+    // sock.close();
 
-    zmq_close (serverSocket);
-    zmq_ctx_destroy (serverContext);
+    zmq_close(serverSocket);
+    zmq_ctx_destroy(serverContext);
 }
 
 //********************************************************************************
@@ -201,13 +196,15 @@ int main()
     std::cout << "\nWaiting for incomming data (" << configval.waiting_incomming_data_sec << "sec)!\n";
 
     std::ofstream fout;
-    if (configval.write_file) fout.open("data_receiver.log");
+    if (configval.write_file)
+        fout.open("data_receiver.log");
 
     std::vector<SOneBuffer> circular_buffer(configval.circular_buffer_num_elements);
 
-    std::thread threadForReceiveFromUDP([&](){receiveFromUDP(configval, circular_buffer);});
-    threadForReceiveFromUDP.detach();
-    
+    std::thread threadForReceiveFromNet([&]()
+                                        { receiveFromNet(configval, circular_buffer); });
+    threadForReceiveFromNet.detach();
+
     auto startTime = std::time(0);
     auto startForWaiting = startTime;
     auto waitingForSec = configval.waiting_incomming_data_sec;
@@ -219,10 +216,10 @@ int main()
         {
             if (circular_buffer[nextElement].isData)
             {
-                uint8_t* buffer_p = circular_buffer[nextElement].buffer;
+                uint8_t *buffer_p = circular_buffer[nextElement].buffer;
 
-                uint16_t dataSize = ntohs(*(uint16_t*)buffer_p);
-                uint32_t numberMsg = ntohl(*(uint32_t*)(buffer_p + size_len));
+                uint16_t dataSize = ntohs(*(uint16_t *)buffer_p);
+                uint32_t numberMsg = ntohl(*(uint32_t *)(buffer_p + size_len));
 
                 // Check for MD5
                 bool md5_check = MD5Check(buffer_p);
@@ -233,7 +230,7 @@ int main()
 
                 // Print to console
                 std::stringstream logstr;
-                std::string timestemp((char*)(buffer_p + size_len + number_len), time_len);
+                std::string timestemp((char *)(buffer_p + size_len + number_len), time_len);
                 logstr << "Processed: #number message=" << numberMsg
                        << "\tSize=" << dataSize
                        << "\tTime=" << timestemp
@@ -261,36 +258,39 @@ int main()
                 // Waiting for some time, expected: 15ms, according to task requirements
                 std::this_thread::sleep_for(std::chrono::milliseconds(configval.process_delay_ms));
 
-                circular_buffer[nextElement].isData = false;                               
-            }            
+                circular_buffer[nextElement].isData = false;
+            }
             processDataInd.store(nextElement);
         }
-        
+
         // Check for break by timeout
         if ((startForWaiting + waitingForSec) < std::time(0))
         {
-            if(startTime == startForWaiting)
+            if (startTime == startForWaiting)
                 std::cout << "\nIt looks like thare was not any data packages!\n";
             break;
         }
     }
-    if (fout.is_open()) fout.close();
+    if (fout.is_open())
+        fout.close();
 
     boost::posix_time::ptime now_stat = boost::posix_time::second_clock::local_time();
     std::string time_stat = to_iso_extended_string(now_stat);
 
     std::stringstream stat;
-    stat << "Data receiver statistics ("<< time_stat <<"):\n";
+    stat << "Data receiver statistics (" << time_stat << "):\n";
     stat << "Received total number of packages: " << recvStat.received_number_packages << "\n";
     stat << "Procecced total number of packages: " << recvStat.procecced_number_packages << "\n";
     stat << "Dropped total number of packages: " << recvStat.dropped_number_packages << "\n";
     stat << "Number of MIN length errors: " << recvStat.errors_min_length << "\n";
     stat << "Number of received length errors: " << recvStat.errors_received_length << "\n";
     stat << "Number of MD5 check errors: " << recvStat.errors_md5_check << "\n";
-    stat << "\nConfiguration:\n" << confS.str();
-    std::cout << std::endl << stat.str();
+    stat << "\nConfiguration:\n"
+         << confS.str();
+    std::cout << std::endl
+              << stat.str();
 
-    fout.open("data_receiver_stat.log");    
+    fout.open("data_receiver_stat.log");
     fout << stat.str();
     fout.close();
 
